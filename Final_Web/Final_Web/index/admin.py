87cf57d697eeb4abbd637718c07fec843444adb0
Imkevinhsu <47855613+Imkevinhsu@users.noreply.github.com>
2019-06-10 19:27:21 +0800

    update

--- Final_Web/Final_Web/index/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Store
-# Register your models here.
-
-#讓admin存取Store資料表
-admin.site.register(Store)
-
--- Final_Web/Final_Web/login/urls.py
+++
@@ -1,4 +0,0 @@
-from django.conf.urls import url
-from . import views
-#未完成，要跟著views.py一起
-urlpatterns=[url(r'^$',views.login,name='login')]
