5d8234a436c35329f141024d168982d5a3b13b87
karta2165539 <32184056+karta2165539@users.noreply.github.com>
2019-06-12 03:33:05 +0800

    update

---
+++ Final_Web_Latest/Final_Web/index/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import url
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+app_name='index'
+urlpatterns=[
+	url(r'^$',views.index,name='index'),
+    #Store的url未完成
+    #url(r'^store/(?P<pk>+)/$',views.store.as_view(),name='stores'),
+    url(r'^shops/(?P<pk>[0-9]+)/$',views.StoreView.as_view(),name='store'),
+    url(r'^shops/$',views.shops,name='shops'),
+    url(r'^statement/$',views.statement,name='statement'),
+    url(r'^fight/$',views.fight,name='fight'),
+    url(r'^addStore/$',views.addStore,name='addstore'),
+    # url(r'^createstore/$',views.crtStore,name='crtstore')
+
+    ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
